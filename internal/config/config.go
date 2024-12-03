8ea5e092bb3f2e56b2093ba63d118207cdcf0138
vivinaumovnaumov <vitamin11182005@gmail.com>
2024-12-03 21:27:40 +0300

    edit config reading

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,20 +1,30 @@
 package config
 
 import (
+	"errors"
+	"event_service/internal/database/postgres"
+
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	GRPCServerPort string `env:"GRPC_SERVER_PORT" env-default:"9090"`
+	postgres.Config
+	MigrationsPath string `env:"MIGRATIONS_PATH"`
+	GRPCServerPort string `env:"GRPC_SERVER_PORT"`
 }
 
-func New() *Config {
+func New() (*Config, error) {
 	cfg := Config{}
-	err := cleanenv.ReadConfig("./configs/config.env", &cfg)
 
-	if err != nil {
-		return nil
+	err := cleanenv.ReadEnv(&cfg)
+
+	if cfg == (Config{}) {
+		return nil, errors.New("config is empty")
 	}
 
-	return &cfg
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
 }
