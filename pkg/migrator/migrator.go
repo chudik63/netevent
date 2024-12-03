b91c6b601897cdd66aff64611f27cc503e5e4e2e
vivinaumovnaumov <vitamin11182005@gmail.com>
2024-12-03 21:26:56 +0300

    implement migrator

---
+++ pkg/migrator/migrator.go
@@ -0,0 +1,28 @@
+package migrator
+
+import (
+	"context"
+	"event_service/internal/config"
+	"event_service/internal/logger"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"go.uber.org/zap"
+)
+
+func Start(ctx context.Context, cfg *config.Config) {
+	l := logger.GetLoggerFromCtx(ctx)
+
+	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cfg.Config.UserName, cfg.Config.Password, cfg.Config.Host, cfg.Config.Port, cfg.Config.DbName)
+
+	m, err := migrate.New("file://"+cfg.MigrationsPath, dbURL)
+	if err != nil {
+		l.Error(ctx, "failed to create migration", zap.String("err", err.Error()))
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		l.Error(ctx, "failed to make migration up", zap.String("err", err.Error()))
+	}
+}
