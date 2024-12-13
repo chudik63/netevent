5a4e1327720da281876b80335f4c3fed257abca1
Vitalya <vitamin11182005@gmail.com>
2024-12-13 21:50:55 +0300

    add kafka message struct

---
+++ event_service/internal/kafka/message.go
@@ -0,0 +1,9 @@
+package kafka
+
+type Message struct {
+	UserEmail  string `json:"user_email"`
+	UserName   string `json:"user_name"`
+	EventName  string `json:"event_name"`
+	EventTime  string `json:"event_time"`
+	EventPlace string `json:"event_place"`
+}
