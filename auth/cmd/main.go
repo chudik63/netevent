423ccb543888170f7421b043100bbefecb0986da
Ilya Vinokurow <ilya.vinokurow@yandex.ru>
2024-12-14 20:00:02 +0300

    add gracefull shutdown

--- auth/cmd/main.go
+++ auth/cmd/main.go
@@ -1,9 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"gitlab.crja72.ru/gospec/go9/netevent/auth/internal/db/postgres"
+	"gitlab.crja72.ru/gospec/go9/netevent/auth/internal/server"
+	logger "gitlab.crja72.ru/gospec/go9/netevent/auth/pkg/loger"
+	"go.uber.org/zap"
+)
+
+var (
+	srvGrpcPort = "5100"
+)
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println(1234)
-	println("asdffasf222222222222")
+	mainLog := logger.New(logger.ServiceName)
+	mainLog.Info("start auth service")
+
+	db, err := postgres.New()
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, logger.ServiceName, mainLog)
+	srv := server.New(ctx, srvGrpcPort, db)
+	go func() {
+		if err := srv.Start(ctx); err != nil {
+			mainLog.Error("err server", zap.Error(err))
+		}
+	}()
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	wg := &sync.WaitGroup{}
+
+	<-ch
+	go func() {
+		srv.Stop(ctx)
+	}()
+
+	// type Hello struct {
+	// 	UserId     int64  `db:"id"`
+	// 	SecondName string `db:"name"`
+	// 	PasswdHash string `db:"password"`
+	// 	Email      string `db:"email"`
+	// 	Role       int    `db:"role"`
+	// 	Interest   string `db:"interest"`
+	// }
+	// var hey Hello
+	// err = db.Db.Get(&hey, "SELECT \"id\" FROM tuser;")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(hey)
 }
