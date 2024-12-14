da885b01e245fb921359439e24549e9efd1d4a35
Ilya Vinokurow <ilya.vinokurow@yandex.ru>
2024-12-14 20:03:57 +0300

    add token

---
+++ auth/internal/token/token_test.go
@@ -0,0 +1,31 @@
+package token
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNewTocken(t *testing.T) {
+	type Data struct {
+		name   string
+		tocken string
+	}
+	data := []Data{
+		Data{
+			name: "user",
+			//tocken: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ`,
+		},
+	}
+	for _, v := range data {
+		toc, err := NewToken(v.name)
+		if err != nil {
+			t.Errorf("error %v", err)
+		}
+		if res, err := ValidTocken(toc); err != nil || !res {
+			t.Errorf("error %v %v", err, res)
+		}
+	}
+
+	toc, _ := NewToken("user")
+	fmt.Println(toc)
+}
