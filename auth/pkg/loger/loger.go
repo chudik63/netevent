5f2343fd283243f6441abab8f5b24c7bf55611df
Ilya Vinokurow <ilya.vinokurow@yandex.ru>
2024-12-14 20:00:56 +0300

    add logger

---
+++ auth/pkg/loger/loger.go
@@ -0,0 +1,60 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+)
+
+const (
+	ServiceName = "auth"
+)
+
+type Logger interface {
+	Info(msg string, fields ...zap.Field)
+	Error(msg string, fields ...zap.Field)
+}
+
+type logger struct {
+	serviceName string
+	logger      *zap.Logger
+}
+
+func (l logger) Info(msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String("ServiceName", ServiceName))
+	l.logger.Info(msg, fields...)
+}
+
+func (l logger) Error(msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String("ServiceName", ServiceName))
+	l.logger.Error(msg, fields...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	zapLogger, _ := zap.NewProduction()
+	defer zapLogger.Sync()
+	fields = append(fields, zap.String("ServiceName", "auth"))
+	zapLogger.Info(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	zapLogger, _ := zap.NewProduction()
+	defer zapLogger.Sync()
+	fields = append(fields, zap.String("ServiceName", "auth"))
+	zapLogger.Error(msg, fields...)
+}
+
+func New(serviceName string) Logger {
+	// zapLogger, _ := zap.NewProduction()
+	zapLogger, _ := zap.NewDevelopment()
+
+	defer zapLogger.Sync()
+	return &logger{
+		serviceName: serviceName,
+		logger:      zapLogger,
+	}
+}
+
+func CtxGetLogger(ctx context.Context) Logger {
+	return ctx.Value(ServiceName).(Logger)
+}
