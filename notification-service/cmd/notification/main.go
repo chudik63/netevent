cfbc6c1956532bb3a61e0768ec13d46b318e7c5c
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-12-15 15:29:17 +0300

    Add graceful shutdown

--- notification-service/cmd/notification/main.go
+++ notification-service/cmd/notification/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	_ "github.com/lib/pq"
 
@@ -10,6 +13,8 @@ import (
 	"gitlab.crja72.ru/gospec/go9/netevent/notification-service/internal/application/notification"
 	"gitlab.crja72.ru/gospec/go9/netevent/notification-service/pkg/logger"
 )
+
+const stopTimeout = 3 * time.Second
 
 func main() {
 	ctx := context.Background()
@@ -31,9 +36,25 @@ func mainWithExitCode(ctx context.Context) (exitCode int) {
 		return 1
 	}
 
-	if err := app.Run(ctx); err != nil {
-		logger.Default().Errorf(ctx, "failed to run app: %s", err)
-		return 1
+	go func() {
+		if err := app.Run(ctx); err != nil {
+			logger.Default().Errorf(ctx, "failed to run app: %s", err)
+			os.Exit(2)
+		}
+	}()
+
+	graceCh := make(chan os.Signal, 1)
+	signal.Notify(graceCh, syscall.SIGINT, syscall.SIGTERM)
+	<-graceCh
+
+	ctx, cancel := context.WithTimeout(ctx, stopTimeout)
+	defer cancel()
+
+	logger.Default().Infof(ctx, "Graceful shutdown...")
+
+	if err := app.Stop(ctx); err != nil {
+		logger.Default().Infof(ctx, "Telegram application shutdown error: %s", err)
+		return 3
 	}
 
 	return 0
