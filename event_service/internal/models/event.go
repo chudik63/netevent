47a2aab25b1f4a54c4950a9e90e5f9cfd2d0d428
Vitalya <vitamin11182005@gmail.com>
2024-12-17 00:18:07 +0300

    remove participant list from event

--- event_service/internal/models/event.go
+++ event_service/internal/models/event.go
@@ -5,12 +5,11 @@ import "time"
 const TimeLayout = time.DateTime
 
 type Event struct {
-	EventID      int64
-	CreatorID    int64
-	Title        string
-	Description  string
-	Time         string
-	Place        string
-	Participants []Participant
-	Topics       []string
+	EventID     int64
+	CreatorID   int64
+	Title       string
+	Description string
+	Time        string
+	Place       string
+	Topics      []string
 }
