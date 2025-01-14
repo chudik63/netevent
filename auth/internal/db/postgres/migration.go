bdc472d27d4b3a9e357688141a8f9a931f3ba497
Vitalya <vitamin11182005@gmail.com>
2025-01-14 14:33:28 +0400

    edit table fields in migration

--- auth/internal/db/postgres/migration.go
+++ auth/internal/db/postgres/migration.go
@@ -9,13 +9,11 @@ import (
 func StartMigration(db *sql.DB) error {
 	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS "tuser" (
 		id serial PRIMARY KEY NOT NULL,
-		name VARCHAR(30),
-		password VARCHAR(30), 
-		role VARCHAR(30),
-		email   VARCHAR(30),
-		interest TEXT,
-		accesstkn TEXT,
-		accessttl INT,
+		name VARCHAR(30) NOT NULL,
+		password VARCHAR(30) NOT NULL, 
+		role VARCHAR(30) NOT NULL,
+		email   VARCHAR(30) NOT NULL,
+		interests TEXT,
 		refreshtkn TEXT,
 		refreshttl INT
 	);`)
