94e266de66690af5020bfe0845136e876d32f8b0
Vitalya <vitamin11182005@gmail.com>
2025-01-14 14:31:41 +0400

    edit field tags in models

--- auth/internal/db/postgres/models/models.go
+++ auth/internal/db/postgres/models/models.go
@@ -1,19 +1,19 @@
 package models
 
 type User struct {
-	Id        int64  `json: "id" db: "id"`
-	Name      string `json: "name" db: "name"`
-	Email     string `json: "email" db: "email"`
-	Password  string `json: "password" db: "password"`
-	Role      string `json: "role" db: "role"`
-	Interests string `json: "interest" db: "interest"`
+	Id        int64  `json:"id" db:"id"`
+	Name      string `json:"name" db:"name"`
+	Email     string `json:"email" db:"email"`
+	Password  string `json:"password" db:"password"`
+	Role      string `json:"role" db:"role"`
+	Interests string `json:"interest" db:"interest"`
 }
 
 type Token struct {
-	AccessTkn  string `json: "accesstkn" db: "accesstkn"`
-	AccessTtl  int64  `json: "accessttl" db: "accessttl"`
-	RefreshTkn string `json: "refreshtkn" db: "refreshtkn"`
-	RefreshTtl int64  `json: "refreshttl" db: "refreshttl"`
+	AccessTkn  string `json:"accesstkn" db:"accesstkn"`
+	AccessTtl  int64  `json:"accessttl" db:"accessttl"`
+	RefreshTkn string `json:"refreshtkn" db:"refreshtkn"`
+	RefreshTtl int64  `json:"refreshttl" db:"refreshttl"`
 }
 
 type Participant struct {
