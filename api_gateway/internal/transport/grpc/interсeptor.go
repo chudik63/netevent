d21464ce7ef6ebe81661845aedcd77571ae44341
Vitalya <vitamin11182005@gmail.com>
2025-01-20 21:43:13 +0400

    improve reading request id in headers

---
+++ api_gateway/internal/transport/grpc/interсeptor.go
@@ -0,0 +1,90 @@
+package grpc
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/chudik63/netevent/api_gateway/internal/client"
+	"github.com/chudik63/netevent/auth_service/pkg/proto"
+	"github.com/chudik63/netevent/events_service/pkg/logger"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+type AuthInterceptor struct {
+	authClient *client.AuthClient
+	logger     logger.Logger
+}
+
+func NewAuthInterceptor(authClient *client.AuthClient, logger logger.Logger) *AuthInterceptor {
+	return &AuthInterceptor{
+		authClient: authClient,
+		logger:     logger,
+	}
+}
+
+func (i *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		if info.FullMethod == "/gateway.Gateway/SignIn" || info.FullMethod == "/gateway.Gateway/SignUp" {
+			return handler(ctx, req)
+		}
+
+		creatorRoutes := map[string]bool{
+			"/gateway.Gateway/CreateEvent": true,
+			"/gateway.Gateway/ReadEvent":   true,
+			"/gateway.Gateway/UpdateEvent": true,
+			"/gateway.Gateway/DeleteEvent": true,
+		}
+
+		md, ok := metadata.FromIncomingContext(ctx)
+
+		if !ok {
+			return nil, errors.New("missing metadata in request")
+		}
+
+		authHeader := md.Get("Authorization")
+		if len(authHeader) == 0 {
+			return nil, errors.New("permission denied")
+		}
+
+		token := strings.TrimSpace(authHeader[0])
+		if !strings.HasPrefix(token, "Bearer ") {
+			return nil, errors.New("invalid authorization header")
+		}
+
+		token = strings.TrimPrefix(token, "Bearer ")
+
+		resp, err := i.authClient.Authorise(ctx, &proto.AuthoriseRequest{
+			Token: token,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		role := resp.GetRole()
+		id := resp.GetId()
+
+		ctx = context.WithValue(ctx, "user_id", id)
+
+		if creatorRoutes[info.FullMethod] && role != "creator" {
+			return nil, errors.New("permission denied")
+		}
+
+		requestID := ""
+		if values := md.Get("X-Request-ID"); len(values) > 0 {
+			requestID = values[0]
+		} else {
+			newUUID, err := uuid.NewUUID()
+			if err == nil {
+				requestID = newUUID.String()
+			}
+		}
+
+		ctx = context.WithValue(ctx, "request_id", requestID)
+
+		return handler(ctx, req)
+	}
+}
---
+++ api_gateway/internal/transport/grpc/server.go
@@ -0,0 +1,82 @@
+package grpc
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/chudik63/netevent/api_gateway/internal/client"
+	"github.com/chudik63/netevent/api_gateway/pkg/api/gateway"
+	"github.com/chudik63/netevent/events_service/pkg/logger"
+
+	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+)
+
+type Server struct {
+	grpcServer *grpc.Server
+	restServer *http.Server
+	listener   net.Listener
+}
+
+func New(ctx context.Context, port, restPort string, authClient *client.AuthClient, eventClient *client.EventClient) (*Server, error) {
+	logs := logger.GetLoggerFromCtx(ctx)
+
+	lis, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		logs.Fatal(ctx, "failed to listen", zap.String("err", err.Error()))
+	}
+
+	interceptor := NewAuthInterceptor(authClient, logs)
+
+	grpcServer := grpc.NewServer()
+
+	reflection.Register(grpcServer)
+	gateway.RegisterGatewayServer(grpcServer, NewGatewayServer(authClient, eventClient))
+
+	mux := runtime.NewServeMux(runtime.WithMetadata(func(ctx context.Context, request *http.Request) metadata.MD {
+		header := request.Header.Get("X-Request-ID")
+		md := metadata.Pairs("x-request-id", header)
+		return md
+	}))
+
+	if err := gateway.RegisterGatewayHandlerServer(context.Background(), mux, NewGatewayServer(authClient, eventClient), interceptor.Unary()); err != nil {
+		logs.Fatal(ctx, "failed to create gatewate handler", zap.String("err", err.Error()))
+		return nil, err
+	}
+	restSrv := &http.Server{
+		Addr:    ":" + restPort,
+		Handler: mux,
+	}
+	return &Server{grpcServer, restSrv, lis}, nil
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	logs := logger.GetLoggerFromCtx(ctx)
+
+	eg := errgroup.Group{}
+
+	eg.Go(func() error {
+		logs.Info(ctx, fmt.Sprintf("gRPC server listening on %s", s.listener.Addr()))
+		return s.grpcServer.Serve(s.listener)
+	})
+
+	eg.Go(func() error {
+		logs.Info(ctx, fmt.Sprintf("Rest server listening on %s", s.restServer.Addr))
+		return s.restServer.ListenAndServe()
+	})
+
+	return eg.Wait()
+}
+
+func (s *Server) Stop(ctx context.Context) error {
+
+	s.grpcServer.GracefulStop()
+	return s.restServer.Shutdown(ctx)
+}
